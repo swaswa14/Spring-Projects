e5b4c52b9e50c1e9c68e3c2405181258add4e776
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-08-07 01:53:54 +0800

    Done

---
+++ Easy/TaskScheduler/src/main/java/ph/joshua/salcedo/taskscheduler/ScheduledTask.java
@@ -0,0 +1,24 @@
+package ph.joshua.salcedo.taskscheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTask {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTask.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 1000)
+    public void reportCurrentTime(){
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+
+
+}
---
+++ Easy/TaskScheduler/src/test/java/ph/joshua/salcedo/taskscheduler/TaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.joshua.salcedo.taskscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
