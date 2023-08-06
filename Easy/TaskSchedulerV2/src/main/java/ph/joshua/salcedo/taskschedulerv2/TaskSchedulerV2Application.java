8653a80e65eaa0e7d2c17c47c798a28fb621fb39
swaswa14 <129421068+swaswa14@users.noreply.github.com>
2023-08-07 02:03:23 +0800

    Task Scheduler 2

---
+++ Easy/TaskSchedulerV2/src/main/java/ph/joshua/salcedo/taskschedulerv2/TaskSchedulerV2Application.java
@@ -0,0 +1,13 @@
+package ph.joshua.salcedo.taskschedulerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskSchedulerV2Application.class, args);
+    }
+
+}
---
+++ Easy/TaskSchedulerV2/src/test/java/ph/joshua/salcedo/taskschedulerv2/TaskSchedulerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.joshua.salcedo.taskschedulerv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
